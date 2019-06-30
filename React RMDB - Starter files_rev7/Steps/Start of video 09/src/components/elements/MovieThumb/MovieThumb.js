044d70a5ce172b9c7bbcf0d5b1beb709baa6bb66
Eliot Sanford <eliot587@gmail.com>
2019-06-30 15:27:26 -0500

    starter files added

---
+++ React RMDB - Starter files_rev7/Steps/Start of video 09/src/components/elements/MovieThumb/MovieThumb.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MovieThumb.css';
+
+const MovieThumb = (props) => {
+  return (
+    <div className="rmdb-moviethumb">
+      <img src={props.image} alt="moviethumb" />
+    </div>
+  )
+}
+
+export default MovieThumb;
