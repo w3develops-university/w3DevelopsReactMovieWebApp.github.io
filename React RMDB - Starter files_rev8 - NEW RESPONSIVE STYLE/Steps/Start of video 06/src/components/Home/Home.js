0a61e6a0279c829b3c1c19b7a1c1238972a001bf
Brandi Cotton <bdcotton52@gmail.com>
2019-07-02 14:14:24 -0500

    updated file from instructor

---
+++ React RMDB - Starter files_rev8 - NEW RESPONSIVE STYLE/Steps/Start of video 06/src/components/Home/Home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import HeroImage from '../elements/HeroImage/HeroImage';
+import SearchBar from '../elements/SearchBar/SearchBar';
+import FourColGrid from '../elements/FourColGrid/FourColGrid';
+import MovieThumb from '../elements/MovieThumb/MovieThumb';
+import LoadMoreBtn from '../elements/LoadMoreBtn/LoadMoreBtn';
+import Spinner from '../elements/Spinner/Spinner';
+import './Home.css';
+
+class Home extends Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <div className="rmdb-home">
+        <HeroImage />
+        <SearchBar />
+        <FourColGrid />
+        <Spinner />
+        <LoadMoreBtn />
+      </div>
+    )
+  }
+}
+
+export default Home;
