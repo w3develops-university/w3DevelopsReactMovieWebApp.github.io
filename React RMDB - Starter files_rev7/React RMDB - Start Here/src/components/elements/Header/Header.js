ffad5e1c6576cda938eccbf77e926a3274873f3d
Eliot Sanford <eliot587@gmail.com>
2019-07-02 19:12:43 -0500

    video 3

---
+++ React RMDB - Starter files_rev7/React RMDB - Start Here/src/components/elements/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="rmdb-header">
+            <div className="rmdb-header-content">
+                <img src="./images/reactMovie_logo.png" alt="rmdb-logo" />
+                <img src="./images/tmdb_logo.png" alt="tmdb-logo" />
+            </div>
+        </div>
+    )
+}
+
+export default Header;
