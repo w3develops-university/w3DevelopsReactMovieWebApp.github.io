c272c468658f64d3ea82737ca917a8ef566537d6
Eliot Sanford <eliot587@gmail.com>
2019-07-05 23:41:42 -0500

    updated config for the api key

--- React RMDB - Starter files_rev7/React RMDB - Start Here/src/components/Home/Home.js
+++ React RMDB - Starter files_rev7/React RMDB - Start Here/src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE} from '../../config';
 import HeroImage from '../elements/HeroImage/HeroImage';
 import SearchBar from '../elements/SearchBar/SearchBar';
 import FourColGrid from '../elements/FourColGrid/FourColGrid';
@@ -10,9 +11,27 @@ import './Home.css';
 
 class Home extends Component {
     state = {
-
+        movies: [],
+        heroimage: null,
+        loading: false,
+        currentPage: 0,
+        totalPages: 0,
+        searchTearm: ''
     }
     
+    componentDidMount(){
+        this.setState({ loading: true });
+        const endpoint = `${API_URL}movie/popular?apikey=${API_KEY }&language=en-US&page=1`;
+        this.fetchItems(endpoint);
+    }
+
+    fetchItems = (endpoint) => {
+        fetch(endpoint)
+            .then(result => result.json())
+            .then(result => {
+                console.log(result)
+            })
+    }
     render() {
         return (
             <div className='rmdb-home'>
