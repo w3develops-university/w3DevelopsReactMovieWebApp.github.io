264dc1022f0a7e7497502ac935751e36202a15df
Eliot Sanford <eliot587@gmail.com>
2019-07-08 21:08:47 -0500

    pushing the existing code through the moviejs video part 1

--- React RMDB - Starter files_rev7/React RMDB - Start Here/src/components/Home/Home.js
+++ React RMDB - Starter files_rev7/React RMDB - Start Here/src/components/Home/Home.js
@@ -92,7 +92,7 @@ class Home extends Component {
                                     clickable={true}
                                     image={element.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${element.poster_path}` : './images/no_image.jpg'}
                                     movieId={element.id}
-                                    movieName={element.orginal_title}
+                                    movieName={element.original_title}
                                 />
                     })}
                 
