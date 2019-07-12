6f3ad4a2dcf8fb7b82151b21045d0eca16e672a3
Eliot Sanford <eliot587@gmail.com>
2019-07-12 02:21:28 -0500

    fixed the errors and added .gitignore files for ds_store

--- React RMDB - Starter files_rev7/React RMDB - Start Here/src/components/elements/Header/Header.js
+++ React RMDB - Starter files_rev7/React RMDB - Start Here/src/components/elements/Header/Header.js
@@ -8,9 +8,10 @@ const Header = () => {
             <div className= "rmdb-header-content">
             <Link to="/">
                 <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="rmdb-logo" />
+            </Link>       
                 <img className="rmdb-tmdb-logo" src="./images/tmdb_logo.png" alt="tmdb-logo" />
 
-            </Link>   
+            
             </div>
         </div>
     )
