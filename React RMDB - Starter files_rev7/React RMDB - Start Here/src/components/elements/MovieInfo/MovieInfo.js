296af7022d66098282dbb0398cbd3145e74cef0d
Eliot Sanford <eliot587@gmail.com>
2019-08-10 16:40:28 -0500

    added thumb to movieinfo.js and link to navigation.js

--- React RMDB - Starter files_rev7/React RMDB - Start Here/src/components/elements/MovieInfo/MovieInfo.js
+++ React RMDB - Starter files_rev7/React RMDB - Start Here/src/components/elements/MovieInfo/MovieInfo.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { IMPORT_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from '../../../config';
+import { IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from '../../../config';
 import FontAwesome from 'react-fontawesome'; 
 import MovieThumb from '../MovieThumb/MovieThumb'; 
 import './MovieInfo.css';
+import { POINT_CONVERSION_COMPRESSED } from 'constants';
 
-const MovieInfo = () => {
+const MovieInfo = (props) => {
     return (
-        <div>
-            MovieInfo
+        <div className="rmdb-movieinfo"
+            style={{
+                background: props.movie.backdrop_path ? `url('${IMAGE_BASE_URL}${BACKDROP_SIZE}/${props.movie.backdrop_path}')` : '#000'
+            }}
+        >
+            <div className="rmdb-movieinfo-content">
+                <div className="rmdb-movieinfo-thumb">
+                    <MovieThumb
+                        image={props.movie.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}{props.movie.poster_path}` : './images/no_image.jpg'}
+                        clickable={false}
+                    />
+                </div>
+            </div>
         </div>
     )
 };
