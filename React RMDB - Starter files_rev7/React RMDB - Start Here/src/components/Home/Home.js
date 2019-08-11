d4bc5312ca0317dba9ea5a6ee12a5739b352b81c
Eliot Sanford <eliot587@gmail.com>
2019-08-11 00:12:31 -0500

    searched for the bug by comparisons, but nothing fixed the movie undef

--- React RMDB - Starter files_rev7/React RMDB - Start Here/src/components/Home/Home.js
+++ React RMDB - Starter files_rev7/React RMDB - Start Here/src/components/Home/Home.js
@@ -8,7 +8,6 @@ import Spinner from '../elements/Spinner/Spinner';
 import LoadMoreBtn from '../elements/LoadMoreBtn/LoadMoreBtn';
 import './Home.css';
 
-
 class Home extends Component {
     state = {
         movies: [],
@@ -34,12 +33,11 @@ class Home extends Component {
             searchTerm
         })
 
-        if(searchTerm ===''){
-            endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+        if (searchTerm === "") {
+            endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
         } else {
-            endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`
+            endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`;
         }
-
         this.fetchItems(endpoint);
     }
 
@@ -48,12 +46,12 @@ class Home extends Component {
         this.setState({ loading: true })
 
         if (this.state.searchTerm === '') {
-            endpoint = `${API_URL}movie/popular?api_key:${API_KEY}&language=en-US&page${this.state.currentPage + 1}`;
+            endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page${this.state.currentPage + 1}`;
         } else {
-            endpoint = `${API_URL}search/movie?api_key:${API_KEY}&language=en-US&query=${this.state.searchTerm}&page=${this.state.currentPage + 1}`;
+            endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${this.state.searchTerm}&page=${this.state.currentPage + 1}`;
+        }
             this.fetchItems(endpoint);
         }
-    }
 
     fetchItems = (endpoint) => {
         fetch(endpoint)
@@ -63,7 +61,7 @@ class Home extends Component {
                 movies: [...this.state.movies, ...result.results],
                 heroImage: this.state.heroImage || result.results[0],
                 loading: false,
-                currentPage: result.pages,
+                currentPage: result.page,
                 totalPages: result.total_pages
             })
         })
@@ -79,30 +77,27 @@ class Home extends Component {
                 text={this.state.heroImage.overview} />
                 <SearchBar callback={this.searchItems} />
             </div> : null }
-            
             <div className="rmdb-home-grid">
                 <FourColGrid
                     header={this.state.searchTerm ? 'Search Result' : 'Popular Movies'}
                     loading= {this.state.loading} 
                 > 
-
-                    {this.state.movies.map( (element, i) => {
-                        return <MovieThumb 
-                                    key={i}
-                                    clickable={true}
-                                    image={element.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${element.poster_path}` : './images/no_image.jpg'}
-                                    movieId={element.id}
-                                    movieName={element.original_title}
-                                />
-                    })}
-                
+                    {this.state.movies.map( (element, i) => (
+                        <MovieThumb 
+                            key={i}
+                            clickable={true}
+                            image={element.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${element.poster_path}` : './images/no_image.jpg'}
+                            movieId={element.id}
+                            movieName={element.original_title}
+                        />
+                    ))}
                 </FourColGrid>
                 {this.state.loading ? <Spinner /> : null}
                 {(this.state.currentPage <= this.state.totalPages && !this.state.loading) ? 
                     <LoadMoreBtn text="Load More" onClick={this.loadMoreItems} />
-                    : null}
+                    : null
+                }
             </div>
-
         </div>
         )
     }
--- React RMDB - Starter files_rev7/React RMDB - Start Here/src/components/elements/MovieInfo/MovieInfo.js
+++ React RMDB - Starter files_rev7/React RMDB - Start Here/src/components/elements/MovieInfo/MovieInfo.js
@@ -9,7 +9,7 @@ const MovieInfo = (props) => {
     return (
         <div className="rmdb-movieinfo"
             style={{
-                background: props.movie.backdrop_path ? `url('${IMAGE_BASE_URL}${BACKDROP_SIZE}/${props.movie.backdrop_path}')` : '#000'
+                background: props.movie.backdrop_path ? `url('${IMAGE_BASE_URL}${BACKDROP_SIZE}${props.movie.backdrop_path}')` : '#000'
             }}
         >
             <div className="rmdb-movieinfo-content">
@@ -18,6 +18,17 @@ const MovieInfo = (props) => {
                         image={props.movie.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}{props.movie.poster_path}` : './images/no_image.jpg'}
                         clickable={false}
                     />
+                </div>
+                <div className="rmdb-movieinfo-text">
+                    <h1>{props.movie.title}</h1> 
+                    <h3>PLOT</h3>
+                    <p>{props.movie.overview}</p>
+                    <h3>IMDB RATING</h3>
+                    <div className="rmdb-rating">
+                        <meter min="0" max="100" optimum="100" low="40" high="70" valu={props.movie.vote_average * 10}></meter>
+                        <p className="rmdb-score">{props.movie.vote_average}</p>
+                    </div>
+                    {props.director}
                 </div>
             </div>
         </div>
